050aca8ac2acab1c27b4fee828ed2c49faa76d11
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-25 12:51:50 +0530

    ADD: Part time employee wage calculated

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,13 +4,25 @@ public class EmployeeWageComputation
 	{
 		int IS_PRESENT = 1;
 		int WAGE_PER_HR = 20;
-		int FULL_WORKING_DAY_HRS = 8;
+		int FULL_TIME_WORKING_HRS = 8;
+		int PART_TIME_WORKING_HRS = 4;
+		int IS_FULL_TIME = 1;
 		int empWage = 0;
-		int checkPresence = (int) Math.floor(Math.random()*10%2);
+		int checkPresence = (int) (Math.random()*10%2);
+		int checkWorkingTime = (int) (Math.random()*10%2);
 		if(IS_PRESENT == checkPresence)
 		{
 			System.out.println("Employee is present");
-			empWage = WAGE_PER_HR * FULL_WORKING_DAY_HRS;
+			if(IS_FULL_TIME == checkWorkingTime)
+			{
+				System.out.println("Employee is full timer");
+				empWage = FULL_TIME_WORKING_HRS * WAGE_PER_HR;
+			}
+			else
+			{
+				System.out.println("Employee is part timer");
+				empWage = PART_TIME_WORKING_HRS * WAGE_PER_HR;
+			}
 		}
 		else
 		{
