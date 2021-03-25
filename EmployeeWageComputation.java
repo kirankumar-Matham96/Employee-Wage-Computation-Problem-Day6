828968b50c09b4a0c63c964c747a258b694c8902
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-25 13:10:27 +0530

    ADD: Calculated Monthly Wage of Employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,33 +2,41 @@ public class EmployeeWageComputation
 {
 	public static void main(String[] args)
 	{
+		//CONSTANTS
 		final int IS_PRESENT = 1;
+		final int IS_FULL_TIME = 1;
 		int WAGE_PER_HR = 20;
 		int FULL_TIME_WORKING_HRS = 8;
 		int PART_TIME_WORKING_HRS = 4;
-		final int IS_FULL_TIME = 1;
-		int empWage = 0;
-		int checkPresence = (int) (Math.random()*10%2);
-		int checkWorkingTime = (int) (Math.random()*10%2);
-		switch(checkPresence)
+		int WORKING_DAYS_IN_MONTH = 20;
+
+		//VARIABLES
+		int dayOfMonth = 0;
+		int empDailyWage = 0;
+		int empMonthlyWage = 0;
+		while(dayOfMonth < WORKING_DAYS_IN_MONTH)
 		{
-			case IS_PRESENT:
-				System.out.println("Employee is present");
-				switch(checkWorkingTime)
-				{
-					case IS_FULL_TIME:
-						System.out.println("Employee is full timer");
-						empWage = FULL_TIME_WORKING_HRS * WAGE_PER_HR;
-						break;
-					default:
-						System.out.println("Employee is part timer");
-						empWage = PART_TIME_WORKING_HRS * WAGE_PER_HR;
-						break;
-				}
-				break;
-			default:
-				System.out.println("Employee is absent");
+			int checkPresence = (int) (Math.random()*10%2);
+			int checkWorkingTime = (int) (Math.random()*10%2);
+			switch(checkPresence)
+			{
+				case IS_PRESENT:
+					switch(checkWorkingTime)
+					{
+						case IS_FULL_TIME:
+							empDailyWage = FULL_TIME_WORKING_HRS * WAGE_PER_HR;
+							break;
+						default:
+							empDailyWage = PART_TIME_WORKING_HRS * WAGE_PER_HR;
+					}
+					empMonthlyWage += empDailyWage;
+					break;
+				default:
+			}
+			dayOfMonth++;
 		}
-		System.out.println("Employee Wage is: "+empWage);
+		System.out.println("Employee monthly wage is: "+empMonthlyWage);
+
+
 	}
 }
