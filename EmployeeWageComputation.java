f34806ff55a46ce0abe66a1e33e8e1a3de7037e2
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-25 13:00:06 +0530

    REFRACTED: Used Switch-Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,31 +2,32 @@ public class EmployeeWageComputation
 {
 	public static void main(String[] args)
 	{
-		int IS_PRESENT = 1;
+		final int IS_PRESENT = 1;
 		int WAGE_PER_HR = 20;
 		int FULL_TIME_WORKING_HRS = 8;
 		int PART_TIME_WORKING_HRS = 4;
-		int IS_FULL_TIME = 1;
+		final int IS_FULL_TIME = 1;
 		int empWage = 0;
 		int checkPresence = (int) (Math.random()*10%2);
 		int checkWorkingTime = (int) (Math.random()*10%2);
-		if(IS_PRESENT == checkPresence)
+		switch(checkPresence)
 		{
-			System.out.println("Employee is present");
-			if(IS_FULL_TIME == checkWorkingTime)
-			{
-				System.out.println("Employee is full timer");
-				empWage = FULL_TIME_WORKING_HRS * WAGE_PER_HR;
-			}
-			else
-			{
-				System.out.println("Employee is part timer");
-				empWage = PART_TIME_WORKING_HRS * WAGE_PER_HR;
-			}
-		}
-		else
-		{
-			System.out.println("Employee is absent");
+			case IS_PRESENT:
+				System.out.println("Employee is present");
+				switch(checkWorkingTime)
+				{
+					case IS_FULL_TIME:
+						System.out.println("Employee is full timer");
+						empWage = FULL_TIME_WORKING_HRS * WAGE_PER_HR;
+						break;
+					default:
+						System.out.println("Employee is part timer");
+						empWage = PART_TIME_WORKING_HRS * WAGE_PER_HR;
+						break;
+				}
+				break;
+			default:
+				System.out.println("Employee is absent");
 		}
 		System.out.println("Employee Wage is: "+empWage);
 	}
